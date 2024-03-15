cc6fd19a19b677abe0413c34b74525133dbd5b26
Gabriel Morandim <augustomorandim@gmail.com>
2024-03-15 01:56:19 -0300

    feat: implementing tray system

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,30 +1,69 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use tauri_plugin_positioner::{Position, WindowExt};
+use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial, NSVisualEffectState};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 fn main() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-    let item = CustomMenuItem::new("item".to_string(), "Item");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd + Q");
 
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(quit)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(hide)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(item);
+    let tray_menu = SystemTrayMenu::new().add_item(quit);
 
-    let tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
-        .system_tray(tray)
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_positioner::init())
+        .system_tray(SystemTray::new().with_menu(tray_menu).with_title("yDown"))
+        .setup(|app| {
+            app.listen_global("quit", |_| {
+                std::process::exit(0);
+            });
+
+            let window = app.get_window("main").unwrap();
+
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(
+                &window,
+                NSVisualEffectMaterial::Menu,
+                Some(NSVisualEffectState::Active),
+                Some(6.0),
+            )
+            .expect("error while applying vibrancy");
+
+            Ok(())
+        })
+        .on_system_tray_event(|app, event| {
+            tauri_plugin_positioner::on_tray_event(app, &event);
+
+            match event {
+                SystemTrayEvent::LeftClick {
+                    position: _,
+                    size: _,
+                    ..
+                } => {
+                    let window = app.get_window("main").unwrap();
+
+                    let _ = window.move_window(Position::TrayCenter);
+
+                    if window.is_visible().unwrap() {
+                        window.hide().unwrap();
+                    } else {
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                }
+
+                SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    _ => {} // Add a wildcard to cover all other possible values
+                },
+                _ => {}
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
